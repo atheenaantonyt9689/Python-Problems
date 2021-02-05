35cee5b8e56bfc39404595db52854d3351c1f7c6
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-02-05 22:27:17 +0530

    inventary managemnt

---
+++ DAY2/inventary-management.py
@@ -0,0 +1,31 @@
+"""inventary managmrntt"""
+from typing import List, Any
+
+
+def create_inventory(items):
+    inventory = dict()
+    add_items(inventory, items)
+    return inventory
+
+
+def add_items(inventory, items):
+    for item in items:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+
+def delete_items(inventory, items):
+    for item in items:
+        if item in inventory:
+            inventory[item] = max(inventory[item] - 1, 0)
+    return inventory
+
+
+def list_inventory(inventory):
+    output: List[Any] = list()
+    item: object
+    for item in sorted(inventory.items()):
+        if item[1] > 0:
+            output.append(item)
+    return output
