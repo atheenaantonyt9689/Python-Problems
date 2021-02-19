f06f31d61aec725fec57833cca0db344323beea7
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-02-19 06:03:49 +0530

    initila commit

---
+++ DAY3/robot_simulator.py
@@ -0,0 +1,13 @@
+EAST = 1
+NORTH =2
+WEST = 3
+SOUTH = 4
+
+class Robot:
+    
+    def __init__ (self,direction=NORTH,X=0,Y=0):
+        self.direction=direction
+        self.x=x
+        self.y=Y
+        self.coordeinates=(self.x,self.y)
+
