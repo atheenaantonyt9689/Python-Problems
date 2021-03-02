0f9244da0bd5703b302982cc355acf97026b8829
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-03-02 19:39:12 +0530

    inheritance problm

--- DAY3/robot_simulator.py
+++ DAY3/robot_simulator.py
@@ -3,26 +3,24 @@ NORTH = 2
 WEST = 3
 SOUTH = 4
 
-
 class Robot:
 
-    def __init__(self, direction=NORTH, x=0, y=0):
-        self.direction = direction
-        self.x = x
-        self.y = y
-        self.coordinates = (self.x, self.y)
 
-    def move(self, instructions):
-        i: object
+    def __init__ (self,direction=NORTH,x=0,y=0):
+        self.direction=direction
+        self.x=x
+        self.y=y
+        self.coordinates=(self.x,self.y)
+
+    def move(self,instructions):
         for i in instructions:
             if self.direction == "R":
                 Robot.turn_right(self)
             elif self.direction == "L":
                 Robot.turn_left(self)
-            elif self.direction == "A":
+            elif self.direction =="A":
                 Robot.advance(self)
-            print(i)
-
+    
     def turn_right(self):
         if self.direction == NORTH:
             self.direction = EAST
@@ -32,7 +30,7 @@ class Robot:
             self.direction = WEST
         elif self.direction == WEST:
             self.direction = NORTH
-
+    
     def turn_left(self):
         if self.direction == NORTH:
             self.direction = WEST
@@ -42,17 +40,33 @@ class Robot:
             self.direction = EAST
         elif self.direction == EAST:
             self.direction = NORTH
-
     def advance(self):
         if self.direction == NORTH:
             self.x = self.x + 1
         if self.direction == SOUTH:
-            self.x = self.x - 1
+            self.x = self.x -1
         if self.direction == EAST:
-            self.y = self.y + 1
+            self.y =self.y+1
         if self.direction == WEST:
-            self.y = self.y - 1
+            self.y =self.y -1
 
+        
 
-robot = Robot(EAST, 1, 1)
+    
+    
+
+robot= Robot(EAST,1,1)
 robot.move("RAALAL")
+
+
+        
+
+
+
+
+
+
+        
+
+
+
---
+++ DAY4/inheritance_sample.py
@@ -0,0 +1,22 @@
+class Book:
+    def __init__(self,title,isbn,author,totalpages):
+        self.title = title 
+        self.isbn = isbn
+        self.author = author
+        self.totalpages = totalpages
+    def publish(self):
+        print("title:",self.title)
+        print("isbn:",self.isbn)
+        print("author:",self.author)
+
+
+#obj1=Book()
+#obj1.publish()
+
+
+   
+    
+    
+    
+        
+
