4f3d13af04e6f52c0f2d9557c0b286db2343e804
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-03-03 14:50:28 +0530

    derived class

--- DAY4/inheritance_sample.py
+++ DAY4/inheritance_sample.py
@@ -1,18 +1,21 @@
 class Book:
-    def __init__(self,title,isbn,author,totalpages):
-        self.title = title 
-        self.isbn = isbn
-        self.author = author
-        self.totalpages = totalpages
-    def publish(self):
-        print("title:",self.title)
-        print("isbn:",self.isbn)
-        print("author:",self.author)
+     def __init__(self , title,isbn,author,totalpages):
+          self.title = title
+          self.isbn = isbn
+          self.author = author
+          self.totalpages = totalpages
+     def publish(self):
+         print(self.title ,self.isbn,self.author, self.totalpages)
+class Magazine(Book):
+     def __init__(self ,title ,isbn,author,totalpages):
+          Book.__init__(self,title,author,isbn,totalpages)
+          self.volium= "vol 1"
+          self.voluim_date="hgjdgjhwdgxdjggxsegdxjhxdgh"
+     def view(self):
+          print("Title:" , self.title ,"\nisbn number:",  self.isbn , " \nAUTHOR :" , self.author,"\nvolium:",self.volium,"\nvolium date",self.voluim_date)
+obj = Magazine("Python" , '28',"ATHEEEE","5566")
 
-
-#obj1=Book()
-#obj1.publish()
-
+obj.view()
 
    
     
