564002793eb81b8f425cd73a3c6e8d3db2c82158
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-03-03 19:22:04 +0530

    super function added

--- DAY4/inheritance_sample.py
+++ DAY4/inheritance_sample.py
@@ -1,25 +1,33 @@
 class Book:
-     def __init__(self , title,isbn,author,totalpages):
-          self.title = title
-          self.isbn = isbn
-          self.author = author
-          self.totalpages = totalpages
-     def publish(self):
-         print(self.title ,self.isbn,self.author, self.totalpages)
+    def __init__(self, title, isbn, author, totalpages):
+        """
+
+        :type isbn: object
+        """
+        self.title = title
+        self.isbn = isbn
+        self.author = author
+        self.totalpages = totalpages
+
+    def publish(self):
+        print(self.title, self.isbn, self.author, self.totalpages)
+
+
 class Magazine(Book):
-     def __init__(self ,title ,isbn,author,totalpages):
-          Book.__init__(self,title,author,isbn,totalpages)
-          self.volium= "vol 1"
-          self.voluim_date="hgjdgjhwdgxdjggxsegdxjhxdgh"
-     def view(self):
-          print("Title:" , self.title ,"\nisbn number:",  self.isbn , " \nAUTHOR :" , self.author,"\nvolium:",self.volium,"\nvolium date",self.voluim_date)
-obj = Magazine("Python" , '28',"ATHEEEE","5566")
+    def __init__(self, title, isbn, author, totalpages, volume, vol_date):
+        super().__init__(title, author, isbn, totalpages)
+        self.volume = volume
+        self.volume_date = vol_date
+
+    def view(self):
+        """
+
+        :rtype: object
+        """
+        print("Title:", self.title, "\n isbn number:", self.isbn, " \nAUTHOR :", self.author, "\nvolume:", self.volume,
+              '\nvolume date', self.volume_date)
+
+
+obj = Magazine("Python", '28', "ATHEEEE", "5566", "myyyyyy", "5732e5676e5723e")
 
 obj.view()
-
-   
-    
-    
-    
-        
-
